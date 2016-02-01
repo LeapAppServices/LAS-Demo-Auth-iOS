b3c90c39579316b90b53640bae595bb14e1783f4
Jin Sun <jsun@ilegendsoft.com>
2016-02-01 15:25:46 +0800

    update to maxleap 2.0.8

--- Frameworks/MaxLeap.framework/Headers/MLMarketingManager.h
+++ Frameworks/MaxLeap.framework/Headers/MLMarketingManager.h
@@ -2,12 +2,14 @@
 //  MLMarketingManager.h
 //  MaxLeap
 //
-//  Created by Sun Jin on 5/29/15.
-//  Copyright (c) 2015 leapas. All rights reserved.
-//
 
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
+#ifdef EXTENSION_IOS
+#   import <MaxLeapExt/MLConstants.h>
+#else
+#   import <MaxLeap/MLConstants.h>
+#endif
+
+NS_ASSUME_NONNULL_BEGIN
 
 /**
  *  The dissmiss button on in-app message view is always on top.
@@ -22,12 +24,12 @@ typedef NS_ENUM(NSUInteger, MLInAppMessageDismissButtonLocation){
 };
 
 /**
- *  Enter point to enable marketing to recieve In-App messages and test In-App messages.
+ *  Enter point to enable marketing to receive In-App messages and test In-App messages.
  */
 @interface MLMarketingManager : NSObject
 
 /** 
- *  Enable the marketing module to recieve In-App messages.
+ *  Enable the marketing module to receive In-App messages.
  */
 + (void)enable;
 
@@ -50,9 +52,10 @@ typedef NS_ENUM(NSUInteger, MLInAppMessageDismissButtonLocation){
 ///--------------------------------------
 
 /** Used to record performance data for push notifications
- @param notificationInfo The dictionary from `didFinishLaunchingWithOptions:` should be passed on to this method
+ @param notificationInfo The dictionary from either `didFinishLaunchingWithOptions:` or
+ `didReceiveRemoteNotification:` should be passed on to this method.
  */
-+ (void)handlePushNotificationOpened:(NSDictionary *)notificationInfo;
++ (void)handlePushNotificationOpened:(nullable NSDictionary *)notificationInfo;
 
 #pragma mark - In-App Message
 ///--------------------------------------
@@ -86,9 +89,12 @@ typedef NS_ENUM(NSUInteger, MLInAppMessageDismissButtonLocation){
  */
 + (MLInAppMessageDismissButtonLocation)inAppMessageDismissButtonLocation;
 
+#pragma mark - In-App Message Debugging
 + (void)triggerInAppMessage:(NSString *)triggerName;
-+ (void)triggerInAppMessage:(NSString *)triggerName withAttributes:(NSDictionary<NSString*, NSString*> *)attributes;
++ (void)triggerInAppMessage:(NSString *)triggerName withAttributes:(nullable NSDictionary ML_GENERIC(NSString*, NSString*) *)attributes;
 
 + (void)dismissCurrentInAppMessage;
 
 @end
+
+NS_ASSUME_NONNULL_END
